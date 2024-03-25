0db8de5a29685285d56ebdea3c4965b983d0b840
Snuslik <144604004+Snuslik@users.noreply.github.com>
2024-03-25 15:11:37 +0300

    Zoopark

---
+++ 8_Horikov/ZOO/Class1.cs
@@ -0,0 +1,134 @@
+﻿namespace ZOO
+{
+    public class Zoo
+    {
+        public interface ISwimmer
+        {
+            string Swim();
+        }
+
+        public interface IWalker
+        {
+            string Walk();
+        }
+
+        public interface IFlyer
+        {
+            string Fly();
+        }
+
+        public interface ISocialInteraction
+        {
+            string Socialize();
+        }
+
+        public class Animal
+        {
+            public string Name;
+            public int Age;
+            public string Species;
+            public string Sound;
+            public bool IsAlive;
+            public string Habitat;
+
+            public Animal(string name, int age, string species, string sound)
+            {
+                Name = name;
+                Age = age;
+                Species = species;
+                Sound = sound;
+                IsAlive = true;
+                Habitat = "Неизвестно";
+            }
+
+            public virtual string MakeSound()
+            {
+                return $"{Name} {Species} издает звук: {Sound}";
+            }
+
+            public virtual string Eat()
+            {
+                return $"{Name} {Species} питается в зоопарке.";
+            }
+
+            public virtual string Sleep()
+            {
+                return $"{Name} {Species} спит в своем уютном вольере.";
+            }
+
+            public virtual string Reproduce()
+            {
+                return $"{Name} {Species} размножается в специальных условиях зоопарка.";
+            }
+        }
+
+        public class Penguin : Animal, ISwimmer
+        {
+            public Penguin(string name, int age) : base(name, age, "Пингвин", "кря")
+            {
+                Habitat = "Вольер для пингвинов";
+            }
+
+            public string Swim()
+            {
+                return $"{Name} пингвин плавает в бассейне внутри вольера.";
+            }
+        }
+
+        public class Monkey : Animal, IWalker
+        {
+            public Monkey(string name, int age) : base(name, age, "Обезьяна", "у-у-у")
+            {
+                Habitat = "Вольер для обезьян";
+            }
+
+            public string Walk()
+            {
+                return $"{Name} обезьяна карабкается и играет в вольере.";
+            }
+        }
+
+        public class Shark : Animal, ISwimmer
+        {
+            public Shark(string name, int age) : base(name, age, "Акула", "жуткий звук")
+            {
+                Habitat = "Аквариум";
+            }
+
+            public string Swim()
+            {
+                return $"{Name} акула плавает в аквариуме зоопарка.";
+            }
+        }
+
+        public class Classmate : Animal, ISocialInteraction
+        {
+            public Classmate(string name, int age) : base(name, age, "Одноклассник", "Привет!")
+            {
+                Habitat = "Классная комната";
+            }
+
+            public string Socialize()
+            {
+                return $"{Name} одноклассник общается с другими детьми в классной комнате.";
+            }
+            public override string MakeSound()
+            {
+                return $"{Name} одноклассник издает звук: {Sound}";
+            }
+        }
+        public class Kangaroo : Animal, IWalker
+        {
+            public Kangaroo(string name, int age) : base(name, age, "Кенгуру", "скак-скак")
+            {
+                Habitat = "Вольер для кенгуру";
+            }
+
+            public string Walk()
+            {
+                return $"{Name} кенгуру ходит и прыгает в вольере.";
+            }
+
+        }
+    }
+}
---
+++ 8_Horikov/Zooooo/Program.cs
@@ -0,0 +1,20 @@
+﻿using static ZOO.Zoo;
+
+Animal[] animals = new Animal[5];
+
+Penguin penguin = new Penguin("Пингвинчик", 5);
+Monkey monkey = new Monkey("Обезьянка", 3);
+Kangaroo kangaroo = new Kangaroo("Сумчатик", 2);
+Shark shark = new Shark("Акула Брусничка", 10);
+Classmate classmate = new Classmate("Друг", 12);
+
+animals[0] = penguin;
+animals[1] = monkey;
+animals[2] = kangaroo;
+animals[3] = shark;
+animals[4] = classmate;
+
+foreach (Animal animal in animals)
+{
+    Console.WriteLine(animal.MakeSound());
+}
